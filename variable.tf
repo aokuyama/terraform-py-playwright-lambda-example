eb9d9738f6402c22c7c93acbd8d41889ef22812e
aokuyama <ao980s@gmail.com>
2022-02-13 08:40:02 +0900

    add ecr registory and codebuild

--- variable.tf
+++ variable.tf
@@ -2,3 +2,25 @@ variable "region" {
   type    = string
   default = "ap-northeast-1"
 }
+
+variable "project_name" {
+  type    = string
+  default = "py-playwright-example"
+}
+variable "ecr_repository_name" {
+  type    = string
+  default = "py-playwright-example"
+}
+variable "tag_daploy" {
+  type    = string
+  default = "main"
+}
+
+variable "uri_repository" {
+  type    = string
+  default = "https://github.com/aokuyama/py-playwright_super_driver.git"
+}
+variable "branch-name_deploy" {
+  type    = string
+  default = "main"
+}
